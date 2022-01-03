37c0d30d7152fce6bc2f986b159087d779bc276e
Jamie Davies <jamie@jagd.me>
2022-01-03 11:41:32 +0000

    Add hover effect to Link component

--- components/Link.tsx
+++ components/Link.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Link = (props: React.DetailedHTMLProps<React.AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement>) =>
   <a
   {...props}
-    className={`relative after:bg-zinc-400 after:dark:bg-zinc-600 after:-bottom-0.5 after:h-px after:absolute after:left-0 after:right-0 ${props.className}`}
+    className={`relative after:bg-zinc-400 after:dark:bg-zinc-600 after:hover:bg-zinc-600 after:hover:dark:bg-zinc-400 after:-bottom-0.5 after:h-px after:absolute after:left-0 after:right-0 ${props.className}`}
   >
     {props.children}
   </a>
