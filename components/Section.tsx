9cda060037d903a91a2145b2fa330dd97a758af0
Jamie Davies <jamie@jagd.me>
2022-01-03 12:04:03 +0000

    Tweak section margins when on smaller screens

--- components/Section.tsx
+++ components/Section.tsx
@@ -6,7 +6,7 @@ export default function Section({
   return (
     <section {...props}>
       {!!title && (
-        <h3 className="text-xs uppercase tracking-widest font-extralight lg:text-right lg:-translate-x-full lg:absolute lg:pt-0.5 lg:pr-5 text-zinc-400 dark:text-zinc-600 transition-colors duration-300 ease-in-out before:content-['//_']">
+        <h3 className="text-xs uppercase tracking-widest font-extralight mb-1.5 lg:mb-0 lg:text-right lg:-translate-x-full lg:absolute lg:pt-0.5 lg:pr-5 text-zinc-400 dark:text-zinc-600 transition-colors duration-300 ease-in-out before:content-['//_']">
           {title}
         </h3>
       )}
