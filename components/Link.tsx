d40766064c84ec1fe325f0ae45e574123d2a4a22
Jamie Davies <jamie@jagd.me>
2023-07-10 22:15:36 +0100

    Tweak section header and link underline colours

--- components/Link.tsx
+++ components/Link.tsx
@@ -7,7 +7,7 @@ const Link = ({
 >) => (
   <a
     {...props}
-    className={`relative after:absolute after:-bottom-0.5 after:left-0 after:right-0 after:h-px after:bg-zinc-400 after:hover:bg-zinc-600 after:dark:bg-zinc-600 after:hover:dark:bg-zinc-400 ${props.className}`}
+    className={`relative after:absolute after:-bottom-0.5 after:left-0 after:right-0 after:h-px after:bg-zinc-400 after:hover:bg-zinc-600 after:dark:bg-zinc-500 after:hover:dark:bg-zinc-300 ${props.className}`}
   >
     {children}
   </a>
--- components/Section.tsx
+++ components/Section.tsx
@@ -6,7 +6,7 @@ export default function Section({
   return (
     <section {...props}>
       {!!title && (
-        <h3 className="mb-1.5 text-xs font-extralight uppercase tracking-widest text-zinc-400 transition-colors duration-300 ease-in-out before:content-['//_'] dark:text-zinc-600 lg:absolute lg:mb-0 lg:-translate-x-full lg:pr-5 lg:pt-0.5 lg:text-right">
+        <h3 className="mb-1.5 text-xs font-extralight uppercase tracking-widest text-zinc-500 transition-colors duration-300 ease-in-out before:content-['//_'] lg:absolute lg:mb-0 lg:-translate-x-full lg:pr-5 lg:pt-0.5 lg:text-right">
           {title}
         </h3>
       )}
